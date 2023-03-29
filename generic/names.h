1e5dff94b2adcebe076aa8a919f2fa19fef48d92
Cyan Ogilvie <cyan.ogilvie@gmail.com>
2023-03-29 07:58:47 +0200

    Factored out names

--- generic/names.h
+++ generic/names.h
@@ -1,10 +1,1 @@
-#ifndef _NAMES_H
-#define _NAMES_H
-#if DEBUG
-
-const char* name(const void *const thing);	// name given thing
-void* thing(const char *const name);		// thing given name
-void names_shutdown();
-
-#endif
-#endif
+../teabase/names.h
